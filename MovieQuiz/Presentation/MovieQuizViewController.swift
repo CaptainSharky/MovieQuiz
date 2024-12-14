4afd4ad52144e5315c924a1ec8e80d7687b720dc
CaptainSharky <chuikostepan@mail.ru>
2024-12-14 16:24:06 +0300

    Добавлены аутлеты и экшены элементов

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -1,72 +1,133 @@
 import UIKit
 
 final class MovieQuizViewController: UIViewController {
+    @IBOutlet private weak var imageView: UIImageView!
+    @IBOutlet private weak var textLabel: UILabel!
+    @IBOutlet private weak var counterLabel: UILabel!
     
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    @IBAction private func yesButton(_ sender: Any) {
+        
+    }
+    
+    @IBAction private func noButton(_ sender: Any) {
+        
+    }
+    
 }
+
+struct QuizQuestion {
+    let image: String
+    let text: String
+    let correctAnswer: Bool
+}
+
+private let questions: [QuizQuestion] = [
+    QuizQuestion(
+        image: "The Godfather",
+        text: "Рейтинг этого фильма больше чем 6?",
+        correctAnswer: true),
+    QuizQuestion(
+        image: "The Dark Knight",
+        text: "Рейтинг этого фильма больше чем 6?",
+        correctAnswer: true),
+    QuizQuestion(
+        image: "Kill Bill",
+        text: "Рейтинг этого фильма больше чем 6?",
+        correctAnswer: true),
+    QuizQuestion(
+        image: "The Avengers",
+        text: "Рейтинг этого фильма больше чем 6?",
+        correctAnswer: true),
+    QuizQuestion(
+        image: "Deadpool",
+        text: "Рейтинг этого фильма больше чем 6?",
+        correctAnswer: true),
+    QuizQuestion(
+        image: "The Green Knight",
+        text: "Рейтинг этого фильма больше чем 6?",
+        correctAnswer: true),
+    QuizQuestion(
+        image: "Old",
+        text: "Рейтинг этого фильма больше чем 6?",
+        correctAnswer: false),
+    QuizQuestion(
+        image: "The Ice Age Adventures of Buck Wild",
+        text: "Рейтинг этого фильма больше чем 6?",
+        correctAnswer: false),
+    QuizQuestion(
+        image: "Tesla",
+        text: "Рейтинг этого фильма больше чем 6?",
+        correctAnswer: false),
+    QuizQuestion(
+        image: "Vivarium",
+        text: "Рейтинг этого фильма больше чем 6?",
+        correctAnswer: false)
+]
+
 
 /*
  Mock-данные
  
  
- Картинка: The Godfather
+ Картинка:
  Настоящий рейтинг: 9,2
- Вопрос: Рейтинг этого фильма больше чем 6?
+ Вопрос:
  Ответ: ДА
  
  
- Картинка: The Dark Knight
+ Картинка:
  Настоящий рейтинг: 9
- Вопрос: Рейтинг этого фильма больше чем 6?
+ Вопрос:
  Ответ: ДА
  
  
- Картинка: Kill Bill
+ Картинка:
  Настоящий рейтинг: 8,1
- Вопрос: Рейтинг этого фильма больше чем 6?
+ Вопрос:
  Ответ: ДА
  
  
- Картинка: The Avengers
+ Картинка:
  Настоящий рейтинг: 8
- Вопрос: Рейтинг этого фильма больше чем 6?
+ Вопрос:
  Ответ: ДА
  
  
- Картинка: Deadpool
+ Картинка:
  Настоящий рейтинг: 8
- Вопрос: Рейтинг этого фильма больше чем 6?
+ Вопрос:
  Ответ: ДА
  
  
- Картинка: The Green Knight
+ Картинка:
  Настоящий рейтинг: 6,6
- Вопрос: Рейтинг этого фильма больше чем 6?
+ Вопрос:
  Ответ: ДА
  
  
- Картинка: Old
+ Картинка:
  Настоящий рейтинг: 5,8
- Вопрос: Рейтинг этого фильма больше чем 6?
+ Вопрос:
  Ответ: НЕТ
  
  
- Картинка: The Ice Age Adventures of Buck Wild
+ Картинка:
  Настоящий рейтинг: 4,3
- Вопрос: Рейтинг этого фильма больше чем 6?
+ Вопрос:
  Ответ: НЕТ
  
  
- Картинка: Tesla
+ Картинка:
  Настоящий рейтинг: 5,1
  Вопрос: Рейтинг этого фильма больше чем 6?
  Ответ: НЕТ
  
  
- Картинка: Vivarium
+ Картинка:
  Настоящий рейтинг: 5,8
  Вопрос: Рейтинг этого фильма больше чем 6?
  Ответ: НЕТ
